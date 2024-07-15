d9584af1f2568326ca312605fd80e315f8ed56a7
AYUSHIPATEL123 <131158716+AYUSHIPATEL123@users.noreply.github.com>
2024-07-15 13:15:24 +0000

    bk

---
+++ basic/hcdv.js
@@ -0,0 +1,25 @@
+console.log("bsaubd")
+let a = 3;
+console.log(a);
+if (a % 2 == 0) {
+    console.log("even noumber");
+    let q=7;
+}
+else {
+    console.log("odd number");
+}
+let q=7;
+console.log(q);
+
+let n=4;
+let c="cvhdsv";
+let b=false;
+let d=undefined;
+let e=null;
+let n1=typeof n;
+let c1=typeof c;
+let b1=typeof b;
+let d1=typeof d;
+let e1=typeof e;
+
+console.table([n,c,b,d,e,n1,c1,b1,d1,e1]);
